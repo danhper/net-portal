d351ab3b40c6d76fd5ef528ff84fd6b20897f76f
xarsh <info@s46.net>
2012-12-30 16:31:30 +0900

    Add all views

--- course_navi/views.py
+++ course_navi/views.py
@@ -4,3 +4,23 @@ from django.http import HttpResponse
 def index(request):
   t = loader.get_template("course_navi/index.html")
   return HttpResponse(t.render(Context({})))
+
+def timetable(request):
+	t = loader.get_template("course_navi/timetable.html")
+	return HttpResponse(t.render(Context({})))
+
+def message(request):
+	t = loader.get_template("course_navi/message.html")
+	return HttpResponse(t.render(Context({})))
+
+def news(request):
+	t = loader.get_template("course_navi/news.html")
+	return HttpResponse(t.render(Context({})))
+
+def settings(request):
+	t = loader.get_template("course_navi/settings.html")
+	return HttpResponse(t.render(Context({})))
+
+def lecture(request):
+	t = loader.get_template("course_navi/lecture.html")
+	return HttpResponse(t.render(Context({})))
--- net_portal/urls.py
+++ net_portal/urls.py
@@ -6,5 +6,10 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^$', 'course_navi.views.index', name='index'),
+    url(r'^timetable$', 'course_navi.views.timetable', name='timetable'),
+    url(r'^message$', 'course_navi.views.message', name='message'),
+    url(r'^news$', 'course_navi.views.news', name='news'),
+    url(r'^settings$', 'course_navi.views.settings', name='settings'),
+    url(r'^class$', 'course_navi.views.lecture', name='lecture'),
     url(r'^login$', 'students.views.login', name='login'),
 )
