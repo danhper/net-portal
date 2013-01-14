8048c7380091ae81134b6ab0a914e118f69bdedd
tuvistavie <tuvistavie@gmail.com>
2013-01-14 22:25:45 +0900

    Add setup.py file.

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='net-portal',
+    version='0.0.1',
+    description='A replacement for Waseda Net Portal',
+    long_description=open('README.md').read(),
+    author='Daniel Perez',
+    author_email='tuvistavie@gmail',
+    url='https://github.com/tuvistavie/net-portal',
+    download_url='https://github.com/tuvistavie/net-portal',
+    license='WTFPL',
+    scripts=['scripts/parse_subjects'],
+    packages=find_packages(),
+    tests_require=[
+        'django>=1.3,<1.5',
+        'psycopg2',
+        'beautifulsoup4',
+        'lxml',
+        'rsa',
+        'django-debug-toolbar'
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: End Users/Desktop',
+        'License :: WTFPL',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP'
+    ],
+)
