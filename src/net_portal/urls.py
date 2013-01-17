f410fb6289288ca6858d7c493eb4ef18224181fa
xarsh <info@s46.net>
2013-01-17 16:54:44 +0900

    Modify routes

--- src/net_portal/urls.py
+++ src/net_portal/urls.py
@@ -7,5 +7,8 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     url(r'^$', 'portal.views.index', name='index'),
     url(r'^', include('students.urls', namespace='students', app_name='students')),
-    url(r'^cnavi/', include('course_navi.urls', namespace='cnavi', app_name='cnavi'))
+    url(r'^cnavi/', include('course_navi.urls', namespace='cnavi', app_name='cnavi')),
+    url(r'^about$', 'portal.views.about', name='about'),
+    url(r'^team$', 'portal.views.team', name='team'),
+    url(r'^contact$', 'portal.views.contact', name='contact')
 )
--- src/portal/views.py
+++ src/portal/views.py
@@ -1,5 +1,15 @@
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.shortcuts import render
 
 def index(request):
     return HttpResponseRedirect(reverse('cnavi:index'))
+
+def about(request):
+    return render(request, "students/about.html")
+
+def team(request):
+    return render(request, "students/team.html")
+
+def contact(request):
+    return render(request, "students/contact.html")
