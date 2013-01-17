74231c28552b174a6ff6c50243fb420478a55609
xarsh <info@s46.net>
2013-01-17 17:23:08 +0900

    Fix title logo design

--- src/portal/views.py
+++ src/portal/views.py
@@ -6,10 +6,10 @@ def index(request):
     return HttpResponseRedirect(reverse('cnavi:index'))
 
 def about(request):
-    return render(request, "students/about.html")
+    return render(request, "net_portal/about.html")
 
 def team(request):
-    return render(request, "students/team.html")
+    return render(request, "net_portal/team.html")
 
 def contact(request):
-    return render(request, "students/contact.html")
+    return render(request, "net_portal/contact.html")
