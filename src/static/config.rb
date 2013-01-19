4e85eea898262747dfe2f9af6e9a0ff6474020a3
xarsh <info@s46.net>
2013-01-19 12:46:05 +0900

    Fix css and sprites

--- src/static/config.rb
+++ src/static/config.rb
@@ -7,16 +7,15 @@ sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
 relative_assets = true
-line_comments = false
 
 # You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
